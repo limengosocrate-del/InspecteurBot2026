91553d022851dbf37c547a09b3eb38b31b9a3875
limengosocrate-del <limengosocrate@gmail.com>
2026-06-21 13:21:47 +0100

    Add event listeners for form input changes

--- forms/f01/js/f01.js
+++ forms/f01/js/f01.js
@@ -3,3 +3,46 @@ document.addEventListener("DOMContentLoaded", () => {
     console.log("F01 chargé");
 
 });
+
+const champs = [
+
+"province",
+"ville",
+"commune",
+"quartier",
+"entreprise",
+"adresse",
+
+"rccm",
+"idnat",
+"nif",
+"cnss",
+
+"telephone",
+"email",
+
+"activite",
+
+"nature",
+
+"creation",
+
+"employeur",
+
+"responsable"
+
+];
+
+champs.forEach(id=>{
+
+const champ=document.getElementById(id);
+
+if(!champ) return;
+
+champ.addEventListener("change",()=>{
+
+console.log(id+" enregistré");
+
+});
+
+});
