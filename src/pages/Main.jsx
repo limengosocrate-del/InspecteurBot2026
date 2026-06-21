dc5359394c5a258491decfd3735b104abf8567b2
limengosocrate-del <limengosocrate@gmail.com>
2026-06-21 21:51:48 +0100

    Update Main.jsx

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -8,18 +8,3 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <App />
   </React.StrictMode>
 );
-
-import Header from "./components/Header";
-import Home from "./components/Home";
-
-function App() {
-  return (
-    <>
-      <Header />
-      <Home />
-    </>
-  );
-}
-
-export default App;
-
